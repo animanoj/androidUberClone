652e9ba397d81342adf130360adbbee4ae6e545b
Anirudh Manoj <amanoj2@illinois.edu>
2017-08-18 13:18:19 -0500

    Update gradle and libraries, remove obselete Parse Server details

--- app/src/main/java/com/example/ani/uberclone/StarterApplication.java
+++ app/src/main/java/com/example/ani/uberclone/StarterApplication.java
@@ -13,9 +13,9 @@ public class StarterApplication extends Application {
         Parse.enableLocalDatastore(this);
 
         Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
-                .applicationId("bcaf45625b511f13e12cdb2e5dd1064c8490c604")
-                .clientKey("8c8dd806c9fe52e6c574ca75874ae8b673d45edd")
-                .server("http://ec2-54-202-196-45.us-west-2.compute.amazonaws.com:80/parse/")
+                .applicationId("")
+                .clientKey("")
+                .server("")
                 .build()
         );
 
